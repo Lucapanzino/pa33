ea6e39453d069635a827e1e32c6350f09f634db8
gianluca <lucapanzino@gmail.com>
2017-10-02 22:11:20 +0200

    textarea fixed;

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,9 +40,9 @@ console.log('ok');
      var text_max = 200;
      $('#count_message').html(text_max + ' remaining');
        $('.text-area').keyup(function() {
-       var text_length = $('.text-area').val().length;
-       var text_remaining = text_max - text_length;
-       $('.count_message').html(text_remaining + ' remaining');
+       var text_length = $(this).val().length;
+       var text_remaining = text_max - text_length; 
+       $(this).siblings('.count_message').eq(0).html(text_remaining + ' remaining');
      });
 
 
